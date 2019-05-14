d913c22461b67d25c3c8198c9442a34c5924a36b
atlanssia <hi@atlanssia.com>
2019-05-14 21:51:45 +0800

    add stop

--- configs/configs_test.go
+++ configs/configs_test.go
@@ -3,10 +3,96 @@ package configs
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"strings"
+	"sync"
 	"testing"
+	"time"
 )
 
-func TestGetConfig(t *testing.T) {
+func TestConfigs_NewConfig(t *testing.T) {
+	c := New()
+	assert.NotNil(t, c)
+	cf := &Config{
+		Host: "123.4.56.78",
+	}
+	newCf := c.newConfig(cf)
+	assert.NotNil(t, newCf)
+	assert.NotEmpty(t, newCf.md5Sum)
+	assert.Equal(t, "d41d8cd98f00b204e9800998ecf8427e", newCf.md5Sum)
+}
+
+func TestConfigs_Register(t *testing.T) {
+	c := New()
+	assert.NotNil(t, c)
+	var wg sync.WaitGroup
+	cf := &Config{
+		Host: "123.4.56.78",
+		Port: 8888,
+		AccessKeyId: "access_key_id_123",
+		AccessKeySecret: "access_key_secret_123",
+		Tenant: "tenantId_123",
+		Group: "group_123",
+		DataId: "dataId123",
+	}
+	err := c.Register(&wg, cf)
+	assert.Nil(t, err)
+}
+
+func TestConfigs_Register_Running(t *testing.T) {
+	c := New()
+	assert.NotNil(t, c)
+	var wg sync.WaitGroup
+	cf := &Config{
+		Host: "123.4.56.78",
+		Port: 8888,
+		AccessKeyId: "access_key_id_123",
+		AccessKeySecret: "access_key_secret_123",
+		Tenant: "tenantId_123",
+		Group: "group_123",
+		DataId: "dataId123",
+	}
+	cf.running = true
+
+	c.configsMap.Store("dataId123", cf)
+	err := c.Register(&wg, cf)
+	assert.NotNil(t, err)
+	assert.True(t, strings.Contains(err.Error(), "running"))
+}
+
+func TestConfigs_Stop(t *testing.T) {
+	c := New()
+	assert.NotNil(t, c)
+	var wg sync.WaitGroup
+	cf := &Config{
+		Host: "123.4.56.78",
+		Port: 8888,
+		AccessKeyId: "access_key_id_123",
+		AccessKeySecret: "access_key_secret_123",
+		Tenant: "tenantId_123",
+		Group: "group_123",
+		DataId: "dataId123",
+	}
+	err := c.Register(&wg, cf)
+	assert.Nil(t, err)
+
+	time.Sleep(1 * time.Second)
+
+	err = c.Stop(&wg)
+	assert.Nil(t, err)
+	wg.Wait()
+}
+
+func TestConfigs_Stop_Failed(t *testing.T) {
+	c := New()
+	assert.NotNil(t, c)
+	var wg sync.WaitGroup
+	cf := &Configs{}
+	c.configsMap.Store("dataId123", cf)
+	err := c.Stop(&wg)
+	assert.NotNil(t, err)
+}
+
+func TestConfigs_GetConfig(t *testing.T) {
 	c := New()
 	assert.NotNil(t, c)
 
@@ -19,7 +105,19 @@ func TestGetConfig(t *testing.T) {
 	assert.Equal(t, "123.4.56.78", v.Host)
 }
 
-func TestBuildRetieveUrl(t *testing.T) {
+func TestConfigs_GetConfig_NotFound(t *testing.T) {
+	c := New()
+	assert.NotNil(t, c)
+
+	cf := &Config{
+		Host: "123.4.56.78",
+	}
+	c.configsMap.Store("dataId1", cf)
+	v := c.getConfig("dataId2")
+	assert.Nil(t, v)
+}
+
+func TestConfigs_BuildRetieveUrl(t *testing.T) {
 	c := New()
 	assert.NotNil(t, c)
 
@@ -27,13 +125,12 @@ func TestBuildRetieveUrl(t *testing.T) {
 		Host: "123.4.56.78",
 		Port: 8888,
 	}
-	v, err := c.buildRetrieveUrl(cf)
-	assert.Nil(t, err)
+	v := c.buildRetrieveUrl(cf)
 	assert.NotNil(t, v)
 	assert.Equal(t, "https://123.4.56.78:8888/nacos/v1/cs/configs", v)
 }
 
-func TestBuildRetieveUrl_Customized(t *testing.T) {
+func TestConfigs_BuildRetieveUrl_Customized(t *testing.T) {
 	rp := &ResourcePaths{
 		ResourceListen: "/abc/1/listener",
 		ResourceGet: "/abd/2",
@@ -47,13 +144,12 @@ func TestBuildRetieveUrl_Customized(t *testing.T) {
 		Host: "123.4.56.78",
 		Port: 8888,
 	}
-	v, err := c.buildRetrieveUrl(cf)
-	assert.Nil(t, err)
+	v := c.buildRetrieveUrl(cf)
 	assert.NotNil(t, v)
 	assert.Equal(t, "https://123.4.56.78:8888/abd/2", v)
 }
 
-func TestBuildListenUrl(t *testing.T) {
+func TestConfigs_BuildListenUrl(t *testing.T) {
 	c := New()
 	assert.NotNil(t, c)
 
@@ -61,13 +157,12 @@ func TestBuildListenUrl(t *testing.T) {
 		Host: "123.4.56.78",
 		Port: 8888,
 	}
-	v, err := c.buildListenUrl(cf)
-	assert.Nil(t, err)
+	v := c.buildListenUrl(cf)
 	assert.NotNil(t, v)
 	assert.Equal(t, "https://123.4.56.78:8888/nacos/v1/cs/configs/listener", v)
 }
 
-func TestBuildListenUrl_Customized(t *testing.T) {
+func TestConfigs_BuildListenUrl_Customized(t *testing.T) {
 	rp := &ResourcePaths{
 		ResourceListen: "/abc/1/listener",
 		ResourceGet: "/abd/2",
@@ -81,13 +176,12 @@ func TestBuildListenUrl_Customized(t *testing.T) {
 		Host: "123.4.56.78",
 		Port: 8888,
 	}
-	v, err := c.buildListenUrl(cf)
-	assert.Nil(t, err)
+	v := c.buildListenUrl(cf)
 	assert.NotNil(t, v)
 	assert.Equal(t, "https://123.4.56.78:8888/abc/1/listener", v)
 }
 
-func TestBuildRequest_Get(t *testing.T) {
+func TestConfigs_BuildRequest_Get(t *testing.T) {
 	c := New()
 	assert.NotNil(t, c)
 
@@ -100,8 +194,7 @@ func TestBuildRequest_Get(t *testing.T) {
 		Group: "group_123",
 	}
 
-	v, err := c.buildRetrieveUrl(cf)
-	assert.Nil(t, err)
+	v := c.buildRetrieveUrl(cf)
 	assert.NotNil(t, v)
 	req, cancel, err := c.buildRequest(cf, "GET", v, "", -1)
 	assert.Nil(t, err)
@@ -113,7 +206,7 @@ func TestBuildRequest_Get(t *testing.T) {
 	assert.NotEmpty(t, req.Header.Get("Spas-Signature"))
 }
 
-func TestBuildRequest_Post(t *testing.T) {
+func TestConfigs_BuildRequest_Post(t *testing.T) {
 	c := New()
 	assert.NotNil(t, c)
 
@@ -126,8 +219,7 @@ func TestBuildRequest_Post(t *testing.T) {
 		Group: "group_123",
 	}
 
-	v, err := c.buildRetrieveUrl(cf)
-	assert.Nil(t, err)
+	v := c.buildRetrieveUrl(cf)
 	assert.NotNil(t, v)
 	req, cancel, err := c.buildRequest(cf, "POST", v, "abc", 10)
 	assert.Nil(t, err)
@@ -140,7 +232,7 @@ func TestBuildRequest_Post(t *testing.T) {
 	assert.Equal(t, "10", req.Header.Get("longpullingtimeout"))
 }
 
-func TestBuildListenRequest(t *testing.T) {
+func TestConfigs_BuildListenRequest(t *testing.T) {
 	c := New()
 	assert.NotNil(t, c)
 
@@ -164,7 +256,7 @@ func TestBuildListenRequest(t *testing.T) {
 	assert.Equal(t, "30000", req.Header.Get("longpullingtimeout"))
 }
 
-func TestBuildRetrieveRequest(t *testing.T) {
+func TestConfigs_BuildRetrieveRequest(t *testing.T) {
 	c := New()
 	assert.NotNil(t, c)
 
