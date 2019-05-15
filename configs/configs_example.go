c45b66568ccd0b7a05c1a04e83874f986a419048
atlanssia <hi@atlanssia.com>
2019-05-15 22:20:21 +0800

    refactor and added example

--- configs/configs_example.go
+++ configs/configs_example.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	cs := New("123.4.56.78", 8888, "access_key_id_123", "access_key_secret_123")
+	//cs.SetLogger(myLogger) // set your owner logger that implemented the interface: configs.Logger
 	var wg sync.WaitGroup
 	cf := &Config{
 		Tenant: "tenantId_123",
