4dd94cd2ab508000edc7a14556a337ec413f2944
mark42 <mengchao.lv@connext.com.cn>
2020-10-03 16:41:36 +0800

    encode params before post method

--- common/http_agent/post.go
+++ common/http_agent/post.go
@@ -16,7 +16,6 @@
 package http_agent
 
 import (
-	"github.com/nacos-group/nacos-sdk-go/util"
 	"net/http"
 	"strings"
 	"time"
@@ -26,8 +25,6 @@ func post(path string, header http.Header, timeoutMs uint64, params map[string]s
 	client := http.Client{}
 	client.Timeout = time.Millisecond * time.Duration(timeoutMs)
 	var body string
-
-	util.EncodingParams(params)
 	for key, value := range params {
 		if len(value) > 0 {
 			body += key + "=" + value + "&"
