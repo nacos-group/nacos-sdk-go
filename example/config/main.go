30d7c1a6d9c5f8e4ff0c805175ef69232f5039da
lzp0412 <641785844@qq.com>
2020-11-20 18:01:30 +0800

    Merge branch 'master' of /Users/lizhipeng/git/go/nacos-sdk-go with conflicts.

--- example/config/main.go
+++ example/config/main.go
@@ -1,78 +1,136 @@
+/*
+ * Copyright 1999-2020 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package main
 
 import (
 	"fmt"
-	"github.com/nacos-group/nacos-sdk-go/clients/config_client"
-	"github.com/nacos-group/nacos-sdk-go/clients/nacos_client"
-	"github.com/nacos-group/nacos-sdk-go/common/constant"
-	"github.com/nacos-group/nacos-sdk-go/common/http_agent"
-	"github.com/nacos-group/nacos-sdk-go/vo"
 	"time"
+
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
 )
 
-var clientConfigTest = constant.ClientConfig{
-	TimeoutMs:           10 * 1000,
-	BeatInterval:        5 * 1000,
-	ListenInterval:      300 * 1000,
-	NotLoadCacheAtStart: true,
-	//Username:            "nacos",
-	//Password:            "nacos",
-}
-
-var serverConfigTest = constant.ServerConfig{
-	IpAddr:      "console.nacos.io",
-	Port:        80,
-	ContextPath: "/nacos",
-}
-
-func cretateConfigClientTest() config_client.ConfigClient {
-	nc := nacos_client.NacosClient{}
-	nc.SetServerConfig([]constant.ServerConfig{serverConfigTest})
-	nc.SetClientConfig(clientConfigTest)
-	nc.SetHttpAgent(&http_agent.HttpAgent{})
-	client, _ := config_client.NewConfigClient(&nc)
-	return client
-}
-
 func main() {
-	client := cretateConfigClientTest()
-	content, _ := client.GetConfig(vo.ConfigParam{
-		DataId: "dataId",
-		Group:  "group",
-	})
-	fmt.Println("config :" + content)
-	_, err := client.PublishConfig(vo.ConfigParam{
-		DataId:  "dataId",
-		Group:   "group",
-		Content: "hello world!"})
-	if err != nil {
-		fmt.Printf("success err:%s", err.Error())
+	sc := []constant.ServerConfig{
+		{
+			IpAddr: "console.nacos.io",
+			Port:   80,
+		},
 	}
-	content = ""
 
-	client.ListenConfig(vo.ConfigParam{
-		DataId: "dataId",
-		Group:  "group",
+	cc := constant.ClientConfig{
+		//NamespaceId:         "53dbce03-b817-4a11-8a99-30f835dd5c7e", //namespace id
+		TimeoutMs:           5000,
+		ListenInterval:      10000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "/tmp/nacos3/log",
+		CacheDir:            "/tmp/nacos3/cache",
+		RotateTime:          "1h",
+		MaxAge:              3,
+		LogLevel:            "debug",
+	}
+
+	client, err := clients.CreateConfigClient(map[string]interface{}{
+		"serverConfigs": sc,
+		"clientConfig":  cc,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	//publish config
+	//config key=dataId+group+namespaceId
+	//_, err = client.PublishConfig(vo.ConfigParam{
+	//	DataId:  "test-data",
+	//	Group:   "test-group",
+	//	Content: "hello world!",
+	//})
+	//_, err = client.PublishConfig(vo.ConfigParam{
+	//	DataId:  "test-data-2",
+	//	Group:   "test-group",
+	//	Content: "hello world!",
+	//})
+	if err != nil {
+		fmt.Printf("PublishConfig err:%+v \n", err)
+	}
+
+	//get config
+	content, err := client.GetConfig(vo.ConfigParam{
+		DataId: "test-data",
+		Group:  "test-group",
+	})
+	fmt.Println("GetConfig,config :" + content)
+
+	//Listen config change,key=dataId+group+namespaceId.
+	err = client.ListenConfig(vo.ConfigParam{
+		DataId: "test-data",
+		Group:  "test-group",
 		OnChange: func(namespace, group, dataId, data string) {
-			fmt.Println("config changed group:" + group + ", dataId:" + dataId + ", data:" + data)
-			content = data
+			fmt.Println("config changed group:" + group + ", dataId:" + dataId + ", content:" + data)
 		},
 	})
 
-	client.ListenConfig(vo.ConfigParam{
-		DataId: "abc",
-		Group:  "DEFAULT_GROUP",
+	err = client.ListenConfig(vo.ConfigParam{
+		DataId: "test-data-2",
+		Group:  "test-group",
 		OnChange: func(namespace, group, dataId, data string) {
-			fmt.Println("config changed group:" + group + ", dataId:" + dataId + ", data:" + data)
+			fmt.Println("config changed group:" + group + ", dataId:" + dataId + ", content:" + data)
 		},
 	})
+	time.Sleep(10 * time.Minute)
 
-	time.Sleep(5 * time.Second)
-	_, err = client.PublishConfig(vo.ConfigParam{
-		DataId:  "dataId",
-		Group:   "group",
-		Content: "abc"})
-
-	select {}
+	//_, err = client.PublishConfig(vo.ConfigParam{
+	//	DataId:  "test-data",
+	//	Group:   "test-group",
+	//	Content: "test-listen",
+	//})
+	//
+	//time.Sleep(2 * time.Second)
+	//
+	//_, err = client.PublishConfig(vo.ConfigParam{
+	//	DataId:  "test-data-2",
+	//	Group:   "test-group",
+	//	Content: "test-listen",
+	//})
+	//
+	//time.Sleep(2 * time.Second)
+	//
+	////cancel config change
+	//err = client.CancelListenConfig(vo.ConfigParam{
+	//	DataId: "test-data",
+	//	Group:  "test-group",
+	//})
+	//
+	//time.Sleep(2 * time.Second)
+	//_, err = client.DeleteConfig(vo.ConfigParam{
+	//	DataId: "test-data",
+	//	Group:  "test-group",
+	//})
+	//time.Sleep(5 * time.Second)
+	//
+	//searchPage, _ := client.SearchConfig(vo.SearchConfigParm{
+	//	Search:   "blur",
+	//	DataId:   "",
+	//	Group:    "",
+	//	PageNo:   1,
+	//	PageSize: 10,
+	//})
+	//fmt.Printf("Search config:%+v \n", searchPage)
 
 }
--- vo/config_param.go
+++ vo/config_param.go
@@ -1,22 +1,30 @@
+/*
+ * Copyright 1999-2020 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package vo
 
-/**
-*
-* @description :
-*
-* @author : codezhang
-*
-* @create : 2019-01-08 10:31
-**/
+type Listener func(namespace, group, dataId, data string)
 
 type ConfigParam struct {
-	DataId   string `param:"dataId"`
-	Group    string `param:"group"`
-	Content  string `param:"content"`
+	DataId   string `param:"dataId"`  //required
+	Group    string `param:"group"`   //required
+	Content  string `param:"content"` //required
 	Tag      string `param:"tag"`
 	AppName  string `param:"appName"`
 	OnChange func(namespace, group, dataId, data string)
-	ListenCloseChan chan struct{}
 }
 
 type SearchConfigParm struct {
