58a3b62b9b69f70a03c24f775f853e233ceccc30
adair peng <1374854359@qq.com>
2020-11-30 21:49:33 +0800

    filter loopback ip (#153)

--- util/common.go
+++ util/common.go
@@ -69,19 +69,30 @@ var localIP = ""
 
 func LocalIP() string {
 	if localIP == "" {
-		addrs, err := net.InterfaceAddrs()
+		netInterfaces, err := net.Interfaces()
 		if err != nil {
-			logger.Errorf("get InterfaceAddress failed,err:%+v", err)
+			logger.Errorf("get Interfaces failed,err:%+v", err)
 			return ""
 		}
-		for _, address := range addrs {
-			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-				if ipnet.IP.To4() != nil {
-					localIP = ipnet.IP.String()
-					logger.Infof("InitLocalIp, LocalIp:%s", localIP)
-					break
+
+		for i := 0; i < len(netInterfaces); i++ {
+			if ((netInterfaces[i].Flags & net.FlagUp) != 0) && ((netInterfaces[i].Flags & net.FlagLoopback) == 0) {
+				addrs, err := netInterfaces[i].Addrs()
+				if err != nil {
+					logger.Errorf("get InterfaceAddress failed,err:%+v", err)
+					return ""
+				}
+				for _, address := range addrs {
+					if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+						localIP = ipnet.IP.String()
+						break
+					}
 				}
 			}
+		}
+
+		if len(localIP) > 0 {
+			logger.Infof("Local IP:%s", localIP)
 		}
 	}
 	return localIP
