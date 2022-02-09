bf71d0d2bd32c2c2b62c22ba188ce85c7b83f346
leyou240 <leyou240@live.cn>
2022-02-09 16:05:11 +0800

    ci add docker nacos server
    * 使用docker环境中的真实nacos server

--- clients/client_factory_test.go
+++ clients/client_factory_test.go
@@ -1,6 +1,7 @@
 package clients
 
 import (
+	"net"
 	"reflect"
 	"testing"
 
@@ -9,18 +10,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSetConfigClient(t *testing.T) {
+func getIntranetIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "127.0.0.1"
+	}
 
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return "127.0.0.1"
+}
+
+func TestSetConfigClient(t *testing.T) {
+	ip := getIntranetIP()
 	sc := []constant.ServerConfig{
 		*constant.NewServerConfig(
-			"console.nacos.io",
-			80,
-			constant.WithScheme("http"),
-			constant.WithContextPath("/nacos")),
+			ip,
+			8848,
+			//constant.WithScheme("http"),
+			//constant.WithContextPath("/nacos"),
+		),
 	}
 
 	cc := *constant.NewClientConfig(
-		constant.WithNamespaceId("e525eafa-f7d7-4029-83d9-008937f9d468"),
+		constant.WithNamespaceId("public"),
 		constant.WithTimeoutMs(5000),
 		constant.WithNotLoadCacheAtStart(true),
 		constant.WithLogDir("/tmp/nacos/log"),
@@ -48,6 +66,41 @@ func TestSetConfigClient(t *testing.T) {
 		})
 		assert.Nil(t, err)
 		assert.True(t, reflect.DeepEqual(nacosClientFromMap, nacosClientFromStruct))
+	})
+
+	t.Run("registry", func(t *testing.T) {
+		client, err := NewNamingClient(
+			vo.NacosClientParam{
+				ClientConfig:  &cc,
+				ServerConfigs: sc,
+			},
+		)
+		if err != nil {
+			t.Fatal(err)
+			return
+		}
+		serviceName := "golang-sms@grpc"
+		_, err = client.RegisterInstance(vo.RegisterInstanceParam{
+			Ip:          "f",
+			Port:        8840,
+			ServiceName: serviceName,
+			Weight:      10,
+			Enable:      true,
+			Healthy:     true,
+			Ephemeral:   true,
+			Metadata:    map[string]string{"idc": "shanghai-xs"},
+		})
+		if err != nil {
+			t.Fatal(err)
+			return
+		}
+		is, err := client.GetService(vo.GetServiceParam{
+			ServiceName: serviceName,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("is %#v", is)
 	})
 
 }
