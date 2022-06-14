dc861b813cf8842711d8861aa7c51b6121f2a9af
sivyer9303 <sivyer9303@163.com>
2022-06-14 10:26:47 +0800

    optimize.fix ut.

--- clients/config_client/limiter_test.go
+++ clients/config_client/limiter_test.go
@@ -1,26 +1,35 @@
 package config_client
 
 import (
-	"sync"
+	"fmt"
 	"testing"
+
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLimiter(t *testing.T) {
-	key := "abc"
-	count := 0
-	var wg sync.WaitGroup
-	for i := 0; i < 25; i++ {
-		wg.Add(1)
-		go func() {
-			limited := IsLimited(key)
-			if !limited {
-				count++
-			}
-			wg.Done()
-		}()
+	client := createConfigClientTest()
+	success, err := client.PublishConfig(vo.ConfigParam{
+		DataId:  localConfigTest.DataId,
+		Group:   "default-group",
+		Content: "hello world"})
+
+	assert.Nil(t, err)
+	assert.True(t, success)
+
+	for i := 0; i <= 10; i++ {
+		content, err := client.GetConfig(vo.ConfigParam{
+			DataId: localConfigTest.DataId,
+			Group:  "default-group"})
+		fmt.Printf("第%v次获取,%v  %v \n", i, content, err)
+		if i > 4 {
+			fmt.Println("limiter test,err : ", err)
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+			assert.Equal(t, "hello world", content)
+		}
 	}
-	wg.Wait()
-	assert.Equal(t, 5, count)
 }
