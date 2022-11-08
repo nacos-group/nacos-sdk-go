2d1a525e563e7fe2986e1d43e92dd8590a2d3dcf
三辰 <gengtuo.ygt@alibaba-inc.com>
2022-11-08 14:34:31 +0800

    [ISSUE #537] Fix naming http client authentication.

--- common/nacos_server/nacos_server_test.go
+++ common/nacos_server/nacos_server_test.go
@@ -17,6 +17,8 @@
 package nacos_server
 
 import (
+	"context"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/http_agent"
 	"testing"
 
 	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
@@ -45,4 +47,111 @@ func Test_getAddressWithoutScheme(t *testing.T) {
 	serverConfigTest.IpAddr = "https://console.nacos.io"
 	assert.Equal(t, "https://console.nacos.io:80", getAddress(serverConfigTest))
 
+}
+
+func buildNacosServer(clientConfig constant.ClientConfig) (*NacosServer, error) {
+	return NewNacosServer(context.Background(),
+		[]constant.ServerConfig{*constant.NewServerConfig("http://console.nacos.io", 80)},
+		clientConfig,
+		&http_agent.HttpAgent{},
+		1000,
+		"")
+}
+
+func TestNacosServer_InjectSignForNamingHttp_NoAk(t *testing.T) {
+	clientConfig := constant.ClientConfig{
+		AccessKey: "123",
+		SecretKey: "321",
+	}
+	server, err := buildNacosServer(clientConfig)
+	if err != nil {
+		t.FailNow()
+	}
+
+	param := make(map[string]string)
+	param["serviceName"] = "s-0"
+	param["groupName"] = "g-0"
+	server.InjectSignForNamingHttp(param, constant.ClientConfig{})
+	assert.Empty(t, param["ak"])
+	assert.Empty(t, param["data"])
+	assert.Empty(t, param["signature"])
+}
+
+func TestNacosServer_InjectSignForNamingHttp_WithGroup(t *testing.T) {
+	clientConfig := constant.ClientConfig{
+		AccessKey: "123",
+		SecretKey: "321",
+	}
+	server, err := buildNacosServer(clientConfig)
+	if err != nil {
+		t.FailNow()
+	}
+
+	param := make(map[string]string)
+	param["serviceName"] = "s-0"
+	param["groupName"] = "g-0"
+	server.InjectSignForNamingHttp(param, clientConfig)
+	assert.Equal(t, "123", param["ak"])
+	assert.Contains(t, param["data"], "@@g-0@@s-0")
+	_, has := param["signature"]
+	assert.True(t, has)
+}
+
+func TestNacosServer_InjectSignForNamingHttp_WithoutGroup(t *testing.T) {
+	clientConfig := constant.ClientConfig{
+		AccessKey: "123",
+		SecretKey: "321",
+	}
+	server, err := buildNacosServer(clientConfig)
+	if err != nil {
+		t.FailNow()
+	}
+
+	param := make(map[string]string)
+	param["serviceName"] = "s-0"
+	server.InjectSignForNamingHttp(param, clientConfig)
+	assert.Equal(t, "123", param["ak"])
+	assert.NotContains(t, param["data"], "@@g-0@@s-0")
+	assert.Contains(t, param["data"], "@@s-0")
+	_, has := param["signature"]
+	assert.True(t, has)
+}
+
+func TestNacosServer_InjectSignForNamingHttp_WithoutServiceName(t *testing.T) {
+	clientConfig := constant.ClientConfig{
+		AccessKey: "123",
+		SecretKey: "321",
+	}
+	server, err := buildNacosServer(clientConfig)
+	if err != nil {
+		t.FailNow()
+	}
+
+	param := make(map[string]string)
+	param["groupName"] = "g-0"
+	server.InjectSignForNamingHttp(param, clientConfig)
+	assert.Equal(t, "123", param["ak"])
+	assert.NotContains(t, param["data"], "@@")
+	assert.Regexp(t, "\\d+", param["data"])
+	_, has := param["signature"]
+	assert.True(t, has)
+}
+
+func TestNacosServer_InjectSignForNamingHttp_WithoutServiceNameAndGroup(t *testing.T) {
+	clientConfig := constant.ClientConfig{
+		AccessKey: "123",
+		SecretKey: "321",
+	}
+	server, err := buildNacosServer(clientConfig)
+	if err != nil {
+		t.FailNow()
+	}
+
+	param := make(map[string]string)
+	server.InjectSignForNamingHttp(param, clientConfig)
+	assert.Equal(t, "123", param["ak"])
+	assert.NotContains(t, param["data"], "@@")
+	assert.Regexp(t, "\\d+", param["data"])
+	_, has := param["signature"]
+	assert.True(t, has)
 }
