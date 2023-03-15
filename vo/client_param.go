5aba88210a1e2055a5b7cb32306aa84bab628e07
binbin0325 <binbin0325@apache.org>
2023-03-15 19:08:26 +0800

    update version

--- vo/client_param.go
+++ vo/client_param.go
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package vo
 
 import "github.com/nacos-group/nacos-sdk-go/v2/common/constant"
