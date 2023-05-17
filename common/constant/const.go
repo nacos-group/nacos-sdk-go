0b9851eb5106030ca3d2cfbbe73fb189bf832781
binbin0325 <binbin0325@apache.org>
2023-05-17 20:39:19 +0800

    v2.2.2

--- common/constant/const.go
+++ common/constant/const.go
@@ -68,7 +68,7 @@ const (
 	KEY_BEAT                    = "beat"
 	KEY_DOM                     = "dom"
 	DEFAULT_CONTEXT_PATH        = "/nacos"
-	CLIENT_VERSION              = "Nacos-Go-Client:v2.2.1"
+	CLIENT_VERSION              = "Nacos-Go-Client:v2.2.2"
 	REQUEST_DOMAIN_RETRY_TIME   = 3
 	SERVICE_INFO_SPLITER        = "@@"
 	CONFIG_INFO_SPLITER         = "@@"
