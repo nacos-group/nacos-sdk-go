4dc7ab33bdfa01c775aab523b333fa80909599d0
Sunrisea <1336317033@qq.com>
2025-02-12 17:15:50 +0800

    fix test bug

--- clients/naming_client/naming_grpc/connection_event_listener_test.go
+++ clients/naming_client/naming_grpc/connection_event_listener_test.go
@@ -1,30 +1,34 @@
 package naming_grpc
 
 import (
+	"github.com/golang/mock/gomock"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients/naming_client/naming_proxy"
+	"github.com/nacos-group/nacos-sdk-go/v2/util"
 	"testing"
 )
 
 func TestRedoSubscribe(t *testing.T) {
-	//ctrl := gomock.NewController(t)
-	//defer ctrl.Finish()
-	//
-	//mockProxy := naming_proxy.NewMockINamingProxy(ctrl)
-	//evListener := NewConnectionEventListener(mockProxy)
-	//
-	//cases := []struct {
-	//	serviceName string
-	//	groupName   string
-	//	clusters    string
-	//}{
-	//	{"service-a", "group-a", ""},
-	//	{"service-b", "group-b", "cluster-b"},
-	//}
-	//
-	//for _, v := range cases {
-	//	fullServiceName := util.GetGroupName(v.serviceName, v.groupName)
-	//	evListener.CacheSubscriberForRedo(fullServiceName, v.clusters)
-	//	mockProxy.EXPECT().Subscribe(v.serviceName, v.groupName, v.clusters)
-	//	evListener.redoSubscribe()
-	//	evListener.RemoveSubscriberForRedo(fullServiceName, v.clusters)
-	//}
+	t.Skip("Skipping test,It failed due to a previous commit and is difficult to modify because of the use of struct type assertions in the code.")
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockProxy := naming_proxy.NewMockINamingProxy(ctrl)
+	evListener := NewConnectionEventListener(mockProxy)
+
+	cases := []struct {
+		serviceName string
+		groupName   string
+		clusters    string
+	}{
+		{"service-a", "group-a", ""},
+		{"service-b", "group-b", "cluster-b"},
+	}
+
+	for _, v := range cases {
+		fullServiceName := util.GetGroupName(v.serviceName, v.groupName)
+		evListener.CacheSubscriberForRedo(fullServiceName, v.clusters)
+		mockProxy.EXPECT().Subscribe(v.serviceName, v.groupName, v.clusters)
+		evListener.redoSubscribe()
+		evListener.RemoveSubscriberForRedo(fullServiceName, v.clusters)
+	}
 }
