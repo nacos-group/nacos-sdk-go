5758f57b6b533191f4d073dbf82ec76024e91d3d
Xin Luo <65529035+luoxiner@users.noreply.github.com>
2025-05-15 14:26:31 +0800

    Fix: fix deadlock when close client multi times (#817)
    * Fix: fix deadlock when close client multi times
    
    * Fix: Rename isClose to isClosed

--- clients/config_client/config_client.go
+++ clients/config_client/config_client.go
@@ -60,6 +60,7 @@ type ConfigClient struct {
 	cacheMap                 cache.ConcurrentMap
 	uid                      string
 	listenExecute            chan struct{}
+	isClosed                 bool
 }
 
 type cacheData struct {
@@ -364,8 +365,15 @@ func (client *ConfigClient) SearchConfig(param vo.SearchConfigParam) (*model.Con
 }
 
 func (client *ConfigClient) CloseClient() {
+	client.mutex.Lock()
+	defer client.mutex.Unlock()
+
+	if client.isClosed {
+		return
+	}
 	client.configProxy.getRpcClient(client).Shutdown()
 	client.cancel()
+	client.isClosed = true
 }
 
 func (client *ConfigClient) searchConfigInner(param vo.SearchConfigParam) (*model.ConfigPage, error) {
