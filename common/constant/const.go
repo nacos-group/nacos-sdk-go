4c7a48010b3a6a28f5242e92b01546870f3204e9
Sunrisea <1336317033@qq.com>
2025-08-14 10:13:53 +0800

    fix version

--- common/constant/const.go
+++ common/constant/const.go
@@ -76,7 +76,7 @@ const (
 	KEY_BEAT                          = "beat"
 	KEY_DOM                           = "dom"
 	DEFAULT_CONTEXT_PATH              = "/nacos"
-	CLIENT_VERSION                    = "Nacos-Go-Client:v2.2.7"
+	CLIENT_VERSION                    = "Nacos-Go-Client:v2.3.2"
 	REQUEST_DOMAIN_RETRY_TIME         = 3
 	SERVICE_INFO_SPLITER              = "@@"
 	CONFIG_INFO_SPLITER               = "@@"
